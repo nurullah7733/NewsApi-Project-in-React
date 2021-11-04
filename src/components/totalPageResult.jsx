118023c9f044254c38894562bb706dd814d539b0
Nurullah <nurullah7733@gmail.com>
2021-11-04 14:25:03 +0600

    some edit

--- src/components/totalPageResult.jsx
+++ src/components/totalPageResult.jsx
@@ -1,9 +1,9 @@
 function TotalPageResult() {
   return (
-    <div  className="d-flex">
+    <div className="d-flex">
       <p className="text-black-50">About 0 Result Found</p>
       <p className="text-black-50 " style={{ marginLeft: "auto" }}>
-        Page 55 of 12
+        Page 0 of 0
       </p>
     </div>
   );
